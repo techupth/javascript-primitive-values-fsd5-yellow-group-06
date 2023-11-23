ee32184b470a7a77aa54b180eabb92934ea88bfd
Am <arphasirisubb@gmail.com>
2023-11-23 16:37:34 +0700

    summit assignment primitive

--- ex-3.js
+++ ex-3.js
@@ -4,3 +4,5 @@ let totalFileSize = 250000;
 let uploadedSize = 30000;
 
 // Start coding here
+
+let uploadProgress = `Total file size is ${totalFileSize} bytes \n Uploaded size is ${uploadedSize} bytes`
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,11 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let percentDiscount = 25;
+let discountAmount =  totalPrice * (percentDiscount / 100)
+
+console.log(discountAmount);
+
+let finalPrice = totalPrice - discountAmount;
+console.log(finalPrice);
